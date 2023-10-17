42584c09b7bd0cba131c7b74d3f8e78595b3dadb
Malev1ch <130825280+Malev1ch@users.noreply.github.com>
2023-10-18 01:46:20 +0600

    added 2 sliders
    added 2 sliders

--- slider.js
+++ slider.js
@@ -1,4 +1,4 @@
-
+// Geeks teachers gallery slider
 const slider = document.querySelector('.slider');
 const prevButton = document.querySelector('.prev-button');
 const nextButton = document.querySelector('.next-button');
@@ -28,5 +28,39 @@ function updateSlider() {
     }
   });
 }
-
 updateSlider();
+// Geeks teachers gallery slider
+
+// Geeks studio gallery slider
+const sliderGeeks = document.querySelector('.slider-geeks-studio');
+  const prevButtonGeeks = document.querySelector('.geeks-prev-button');
+  const nextButtonGeeks = document.querySelector('.geeks-next-button');
+  const slidesGeeks = Array.from(sliderGeeks.querySelectorAll('img'));
+  const slideCountGeeks = slidesGeeks.length;
+  let slideIndexGeeks = 0;
+
+  prevButtonGeeks.addEventListener('click', showPreviousSlideGeeks);
+  nextButtonGeeks.addEventListener('click', showNextSlideGeeks);
+
+  function showPreviousSlideGeeks() {
+    slideIndexGeeks = (slideIndexGeeks - 1 + slideCountGeeks) % slideCountGeeks;
+    updateSliderGeeks();
+  }
+
+  function showNextSlideGeeks() {
+    slideIndexGeeks = (slideIndexGeeks + 1) % slideCountGeeks;
+    updateSliderGeeks();
+}
+  
+  function updateSliderGeeks() {
+    slidesGeeks.forEach((slide, index) => {
+      if (index === slideIndexGeeks) {
+        slide.style.display = 'block';
+      } else {
+        slide.style.display = 'none';
+      }
+    });
+  }
+updateSliderGeeks();
+  
+// Geeks studio gallery slider
